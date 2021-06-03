2bf00bf7996adc6200a963742187cbd5806ef390
Anggito Suryo Baskoro <e41191560@student.polije.ac.id>
2021-06-03 23:35:42 +0700

    project mobile

---
+++ Project_Mobile_Rudibonsai/app/src/main/java/com/anggito/project_mobile_rudibonsai/MainActivity.java
@@ -0,0 +1,14 @@
+package com.anggito.project_mobile_rudibonsai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
