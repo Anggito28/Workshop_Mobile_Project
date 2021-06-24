24cc2211c94e6e627ad2820fa201b1ed453457e3
Danang Ardianto <ardhyanto6629@gmail.com>
2021-06-24 16:26:01 +0700

    tampilkan keranjang

---
+++ Rudibonsai/app/src/main/java/com/kelompok2/rudibonsai/model/cart/CartItemQuantity.java
@@ -0,0 +1,14 @@
+package com.kelompok2.rudibonsai.model.cart;
+
+public class CartItemQuantity {
+
+    private int quantity;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
