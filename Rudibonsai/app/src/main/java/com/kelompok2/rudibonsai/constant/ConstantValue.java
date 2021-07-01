fc5631e9d138f8c0d20ff42d8893474574d52f43
Danang Ardianto <ardhyanto6629@gmail.com>
2021-07-01 13:59:19 +0700

    tes ignore ConstantValue

--- Rudibonsai/app/src/main/java/com/kelompok2/rudibonsai/constant/ConstantValue.java
+++
@@ -1,6 +0,0 @@
-package com.kelompok2.rudibonsai.constant;
-
-public class ConstantValue {
-
-    public static final String BASE_URL = "http://192.168.0.110/api/";
-}
---
+++ Rudibonsai/app/src/main/java/com/kelompok2/rudibonsai/constant/ConstantValueSample.java
@@ -0,0 +1,12 @@
+package com.kelompok2.rudibonsai.constant;
+
+public class ConstantValueSample {
+
+//    - Duplikat / copas file ini
+//    - Ganti name file duplikatnya menjadi "ConstantValue"
+//    - Ubah bagian "IP_ADDRESS" di "ConstantValue" menjadi alamat ip laptop / laravel
+//
+//    - File "ConstantValueSample" JANGAN DIHAPUS DAN JANGAN DIUBAH!!!
+
+    public static final String BASE_URL = "http://IP_ADDRESS/api/";
+}
