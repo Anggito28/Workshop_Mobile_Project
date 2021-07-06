0188a337b628002f713cabbf690a47a7b3f8b8c3
Anggito Suryo Baskoro <e41191560@student.polije.ac.id>
2021-07-06 19:58:35 +0700

    register

---
+++ Rudibonsai/app/src/main/java/com/kelompok2/rudibonsai/ui/register/RegisterActivitySecond.java
@@ -0,0 +1,16 @@
+package com.kelompok2.rudibonsai.ui.register;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kelompok2.rudibonsai.R;
+
+public class RegisterActivitySecond extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_second);
+    }
+}
