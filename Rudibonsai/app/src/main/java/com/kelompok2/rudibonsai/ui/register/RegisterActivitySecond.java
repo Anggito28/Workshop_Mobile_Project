f0c6aff7161b8384c5da3806d247570df1523dbf
Anggito Suryo Baskoro <e41191560@student.polije.ac.id>
2021-07-22 22:44:25 +0700

    java not fix register

--- Rudibonsai/app/src/main/java/com/kelompok2/rudibonsai/ui/register/RegisterActivitySecond.java
+++ Rudibonsai/app/src/main/java/com/kelompok2/rudibonsai/ui/register/RegisterActivitySecond.java
@@ -2,6 +2,7 @@ package com.kelompok2.rudibonsai.ui.register;
 
 import android.os.Bundle;
 import android.text.Editable;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
@@ -13,10 +14,10 @@ import com.kelompok2.rudibonsai.R;
 
 public class RegisterActivitySecond extends AppCompatActivity {
     ActionBar actionBar;
-    EditText etname, etphone, etaddress;
-    RadioGroup etgender;
-    Button etprov, etkab, etkec;
+    EditText etname, etphone, etaddress, etpilihprov, etpilihkab, etpilihkec;
     String name, phone, gender, address, prov, kab, kec;
+    RadioGroup etgender;
+    Button btn_daftar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +30,25 @@ public class RegisterActivitySecond extends AppCompatActivity {
         etname = findViewById(R.id.et_name);
         etphone = findViewById(R.id.et_phone);
         etaddress = findViewById(R.id.et_address);
+        etpilihprov = findViewById(R.id.pilihprov);
+        etpilihkab = findViewById(R.id.pilihkab);
+        etpilihkec = findViewById(R.id.pilihkec);
+//        etgender = findViewById(R.id.et_gender);
 
         name = etname.getText().toString();
         phone = etphone.getText().toString();
         address = etaddress.getText().toString();
+        prov = etpilihprov.getText().toString();
+        kab = etpilihkab.getText().toString();
+        kec = etpilihkec.getText().toString();
+//        gender = etgender
+
+        btn_daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
 
     }
 }
